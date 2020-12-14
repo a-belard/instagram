060e9d63b1a5ea80a57a240fb26a5f52c711a41b
belix-pro <66983805+belix-pro@users.noreply.github.com>
2020-12-14 06:19:10 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ async function server(){
     const express = require('express')
     const cors = require('cors')
     const mongoose = require('mongoose')
+    const path = require('path')
 
     const app = express()
 
@@ -39,6 +40,14 @@ async function server(){
     })
     .then(() => console.log("MongoDB Connection Successful"))
     .catch((error) => console.log(error.message))
+
+    if(process.env.NODE_ENV === "production"){
+        app.use(express.static('client.build'))
+
+        app.get('*', (req,res) => {
+            res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+        })
+    }
 
     const port = process.env.PORT || 5000
 
