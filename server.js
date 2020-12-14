c6b58a8eb7d0be23966d6d597af1b4e625f86def
belix-pro <66983805+belix-pro@users.noreply.github.com>
2020-12-14 10:26:25 +0200

    Update server.js

--- server.js
+++ server.js
@@ -39,14 +39,12 @@ async function server(){
     .then(() => console.log("MongoDB Connection Successful"))
     .catch((error) => console.log(error.message))
 
-    if(process.env.NODE_ENV === "production"){
-        app.use(express.static('client.build'))
-
-        app.get('*', (req,res) => {
-            res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-        })
+    if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+        app.use(express.static('client/build'));
+        app.get('*', (req, res) => {
+          res.sendFile(path.join(__dirname + '/client/build/index.html'));
+        });
     }
-
     const port = process.env.PORT || 5000
 
     app.listen(port, () => console.log(`The server started on port ${port}`))
