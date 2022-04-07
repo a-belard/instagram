a5bdd2689d311121597404dc89228ee16f47342f
A. Belard <angebelard@Gmail.com>
2022-04-07 23:25:55 +0200

    Update posts.router.js

--- routes/get_routers/posts.router.js
+++ routes/get_routers/posts.router.js
@@ -4,7 +4,6 @@ const Post = require('../../models/post.model')
 router.get('/posts:email',async (req,res) => {
     try {
         const email = req.params.email.slice(1)
-    
         await Post.find({email})
         .then(posts => res.json(posts))
         .catch(error => res.status(400).json({msg: error.message}))
